65fd54387eab046d80bc3a8b1d17f64561a86a7b
Valeria Meza <valeriameza173@gmail.com>
2021-03-30 15:01:03 -0500

    add components needed for home page.

--- src/app/pages/home/home-details/home-details.component.ts
+++ src/app/pages/home/home-details/home-details.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-details',
   templateUrl: './home-details.component.html',
-  styleUrls: ['./home-details.component.scss']
+  styleUrls: ['./home-details.component.scss'],
 })
 export class HomeDetailsComponent implements OnInit {
+  fullName = 'Valeria Cuevas Meza';
+  hour: number = new Date().getHours();
+  greeting: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.getCurrentHour();
   }
 
+  getCurrentHour(): void {
+    if (this.hour < 12) {
+      this.greeting = 'morning';
+    } else if (this.hour >= 12 && this.hour <= 17) {
+      this.greeting = 'afternoon';
+    } else if (this.hour > 17 && this.hour <= 24) {
+      this.greeting = 'evening';
+    }
+  }
 }
