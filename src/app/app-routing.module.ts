038d2916875f78ed6683e3704045ff8e8e736d0b
Valeria Meza <valeriameza173@gmail.com>
2021-03-30 14:38:29 -0500

    add header links and setup correct routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectComponent } from './pages/project/project.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent },
-  // { path: 'about', component: AboutComponent },
-  // { path: 'projects', component: ProjectComponent },
-  // { path: 'contact', component: ContactComponent },
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
